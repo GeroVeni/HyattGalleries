02682c7abfcbb6667641d871971dbc9ba08c552d
GeroVeni <geroveni@gmail.com>
2019-10-21 05:28:15 +0100

    Add project files.

---
+++ HyattGalleries/HyattGalleries/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using HyattGalleries.Models;
+
+namespace HyattGalleries.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
