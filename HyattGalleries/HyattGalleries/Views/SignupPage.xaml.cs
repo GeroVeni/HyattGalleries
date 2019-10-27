c0adfaaea9b24956ad6e3a1a284e72e895fa2ec3
GeroVeni <geroveni@gmail.com>
2019-10-27 20:59:12 +0000

    Add simple signup page

--- HyattGalleries/HyattGalleries/Views/SignupPage.xaml.cs
+++ HyattGalleries/HyattGalleries/Views/SignupPage.xaml.cs
@@ -15,6 +15,19 @@ namespace HyattGalleries.Views
 		public SignupPage ()
 		{
 			InitializeComponent ();
+
+            NavigationPage.SetHasNavigationBar(this, false);
+
+            usernameEntry.Completed += (s, e) => nameEntry.Focus();
+            nameEntry.Completed += (s, e) => emailEntry.Focus();
+            emailEntry.Completed += (s, e) => passwordEntry.Focus();
+            passwordEntry.Completed += (s, e) => password2Entry.Focus();
+            password2Entry.Completed += (s, e) => OnSignupButtonPressed(s, e);
 		}
+
+        private async void OnSignupButtonPressed(object sender, EventArgs e)
+        {
+
+        }
 	}
 }
