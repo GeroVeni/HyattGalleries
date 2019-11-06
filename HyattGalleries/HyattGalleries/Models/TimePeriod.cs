eba9ee4ec9c7914dbe00707308a549d1f2e0796a
GeroVeni <geroveni@gmail.com>
2019-11-06 03:51:48 +0000

    Add TimePeriod model.

---
+++ HyattGalleries/HyattGalleries/Models/TimePeriod.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HyattGalleries.Models
+{
+    class TimePeriod
+    {
+        public string Name { get; set; }
+
+        public TimePeriod (string Name) { this.Name = Name; }
+    }
+}
