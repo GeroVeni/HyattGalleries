92d4982f9f67d2d74041ce840aae214227e7d7d0
GeroVeni <geroveni@gmail.com>
2019-11-12 04:49:19 +0000

    Add collection views in explore view.

---
+++ HyattGalleries/HyattGalleries/ViewModels/ExploreViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HyattGalleries.ViewModels
+{
+    /// <summary>
+    /// ViewModel for the explore page.
+    /// </summary>
+    class ExploreViewModel
+    {
+        public List<RouteInfo> PreselectedRoutes { get; set; }
+        public List<ExhibitInfo> PopularExhibits { get; set; }
+
+        public ExploreViewModel ()
+        {
+            PreselectedRoutes = new List<RouteInfo>();
+            for (int i = 0; i < 5; i++) PreselectedRoutes.Add(new RouteInfo { imageSource = "", name = "test route" });
+
+            PopularExhibits = new List<ExhibitInfo>();
+            for (int i = 0; i < 5; i++) PopularExhibits.Add(new ExhibitInfo { imageSource = "", name = "test exhibit" });
+        }
+
+        /// <summary>
+        /// Helper class for storing information about an exhibit.
+        /// </summary>
+        public class ExhibitInfo
+        {
+            public string imageSource;
+            public string name { get; set; }
+        }
+
+        /// <summary>
+        /// Helper class for storing information about a preselected route.
+        /// </summary>
+        public class RouteInfo
+        {
+            public string imageSource;
+            public string name { get; set; }
+        }
+    }
+
+    
+}
--- HyattGalleries/HyattGalleries/Views/ExplorePage.xaml.cs
+++ HyattGalleries/HyattGalleries/Views/ExplorePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HyattGalleries.ViewModels;
+using System;
 using System.Collections.Generic;
 
 using Xamarin.Forms;
@@ -7,9 +8,13 @@ namespace HyattGalleries.Views
 {
     public partial class ExplorePage : ContentPage
     {
+        ExploreViewModel viewModel;
+
         public ExplorePage()
         {
             InitializeComponent();
+
+            BindingContext = viewModel = new ExploreViewModel();
         }
     }
 }
