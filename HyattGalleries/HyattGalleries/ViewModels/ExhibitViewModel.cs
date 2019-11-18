940a1adfe2e7cb6acae18f35632f5389467071fe
GeroVeni <geroveni@gmail.com>
2019-11-18 21:19:06 +0000

    Add ViewModel for exhibit view.

---
+++ HyattGalleries/HyattGalleries/ViewModels/ExhibitViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HyattGalleries.ViewModels
+{
+    /// <summary>
+    /// View model for the exhibit page.
+    /// </summary>
+    class ExhibitViewModel
+    {
+        public string ImageSource { get; set; }
+        public string ExhibitName { get; set; }
+        public string ExhibitDescription { get; set; }
+
+        public ExhibitViewModel() {}
+    }
+}
