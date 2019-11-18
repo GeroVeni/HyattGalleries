807e809e8035468d595e2983170a962e83bcfcce
GeroVeni <geroveni@gmail.com>
2019-11-18 21:25:00 +0000

    Create temporary scanner button page.

---
+++ HyattGalleries/HyattGalleries/Views/ScannerPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using HyattGalleries.Services;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HyattGalleries.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ScannerPage : ContentPage
+	{
+		public ScannerPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private async void OnScanClicked(object sender, EventArgs e)
+        {
+            // TODO: Launch scanner when explore button is pressed.
+            LaunchScanner();
+        }
+
+        private async void LaunchScanner()
+        {
+            try
+            {
+                var scanner = DependencyService.Get<IQrScanningService>();
+                var result = await scanner.ScanAsync();
+                // TODO: Handle case when result is null
+                if (result != null)
+                {
+                    Debug.WriteLine("Result is: " + result);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error during qrcode scanning:" + ex.Message);
+                // TODO: Return that scanning failed.
+            }
+        }
+    }
+}
--- HyattGalleries/HyattGalleries/Views/SignupPage.xaml.cs
+++ HyattGalleries/HyattGalleries/Views/SignupPage.xaml.cs
@@ -39,26 +39,5 @@ namespace HyattGalleries.Views
             //await Navigation.PushAsync(new WelcomePage());
             await Navigation.PushAsync(new FilterPage());
         }
-
-        // DO NOT DELETE
-        // Test code for Launching the QRcode scanner
-        //private async void OnSignupWithFbButtonPressed(object sender, EventArgs e)
-        //{
-        //    try
-        //    {
-        //        var scanner = DependencyService.Get<IQrScanningService>();
-        //        var result = await scanner.ScanAsync();
-        //        // TODO: Handle case when result is null
-        //        if (result != null)
-        //        {
-        //            Debug.WriteLine("Result is: " + result);
-        //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        Debug.WriteLine("Error during qrcode scanning:" + ex.Message);
-        //        // TODO: Return that scanning failed.
-        //    }
-        //}
 	}
 }
